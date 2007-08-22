42b9e56e70f4a3098983a28219a2472301d46188
egonw <egonw@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-08-22 09:11:43 +0000

    Placed my GPL3 Operator scripts online

---
+++ operator/branches/1.1/sechemtic_rdfa_operator.js
@@ -0,0 +1,106 @@
+/* Version 1.1                                                                */
+/*                                                                            */
+/* Copyright (C) 2007 Egon Willighagen                                        */
+/* Released under the GPL v3 license                                          */
+/* http://www.gnu.org/copyleft/gpl.html                                       */
+
+var google_inchi = {
+  description: "Search with Google",
+  shortDescription: "Google",
+  scope: {
+    semantic: {
+      "RDF" :  {
+        property : "http://www.blueobelisk.org/chemistryblogs/inchi",
+        defaultNS : "http://www.blueobelisk.org/chemistryblogs/"
+      }
+    }
+  },
+  doAction: function(semanticObject, semanticObjectType) {
+    if (semanticObjectType == "RDF") {
+      return "http://www.google.com/search?q=" + semanticObject.inchi;
+    }
+  }
+};
+
+var pubchem_inchi = {
+  description: "Search in PubChem",
+  shortDescription: "PubChem",
+  icon: "http://pubchem.ncbi.nlm.nih.gov/favicon.ico",
+  scope: {
+    semantic: {
+      "RDF" :  {
+        property : "http://www.blueobelisk.org/chemistryblogs/inchi",
+        defaultNS : "http://www.blueobelisk.org/chemistryblogs/"
+      }
+    }
+  },
+  doAction: function(semanticObject, semanticObjectType) {
+    if (semanticObjectType == "RDF") {
+      return "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?CMD=search&DB=pccompound&term=%22" + semanticObject.inchi + "%22[InChI]";
+    }
+  }
+};
+
+var chemspider_inchi = {
+  description: "Search in ChemSpider",
+  shortDescription: "ChemSpider",
+  icon: "http://www.chemspider.com/favicon.ico",
+  scope: {
+    semantic: {
+      "RDF" :  {
+        property : "http://www.blueobelisk.org/chemistryblogs/inchi",
+        defaultNS : "http://www.blueobelisk.org/chemistryblogs/"
+      }
+    }
+  },
+  doAction: function(semanticObject, semanticObjectType) {
+    if (semanticObjectType == "RDF") {
+      return "http://www.chemspider.com/Search.aspx?q=" + semanticObject.inchi;
+    }
+  }
+};
+
+var pubchem_smiles = {
+  description: "Search in PubChem",
+  shortDescription: "PubChem",
+  icon: "http://pubchem.ncbi.nlm.nih.gov/favicon.ico",
+  scope: {
+    semantic: {
+      "RDF" :  {
+        property : "http://www.blueobelisk.org/chemistryblogs/smiles",
+        defaultNS : "http://www.blueobelisk.org/chemistryblogs/"
+      }
+    }
+  },
+  doAction: function(semanticObject, semanticObjectType) {
+    if (semanticObjectType == "RDF") {
+      return "http://pubchem.ncbi.nlm.nih.gov/search/?smarts=" + semanticObject.smiles;
+    }
+  }
+};
+
+var eMolecules_smiles = {
+  description: "Search in eMolecules",
+  shortDescription: "eMolecules",
+  icon: "http://www.emolecules.com/favicon.ico",
+  scope: {
+    semantic: {
+      "RDF" :  {
+        property : "http://www.blueobelisk.org/chemistryblogs/smiles",
+        defaultNS : "http://www.blueobelisk.org/chemistryblogs/"
+      }
+    }
+  },
+  doAction: function(semanticObject, semanticObjectType) {
+    if (semanticObjectType == "RDF") {
+      return "http://www.emolecules.com/cgi-bin/search?t=ss&q=" + semanticObject.smiles;
+    }
+  }
+};
+
+SemanticActions.add("google_inchi", google_inchi);
+SemanticActions.add("chemspider_inchi", chemspider_inchi);
+SemanticActions.add("pubchem_inchi", pubchem_inchi);
+SemanticActions.add("pubchem_smiles", pubchem_smiles);
+SemanticActions.add("eMolecules_smiles", eMolecules_smiles);
+
