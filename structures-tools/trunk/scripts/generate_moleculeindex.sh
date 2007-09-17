55a81b0d221611a54f4abce6ede67c96dab9ac5d
pansanel <pansanel@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-09-17 09:51:54 +0000

    Updating script to fit the new index.xml syntax

--- structures-tools/trunk/scripts/generate_moleculeindex.sh
+++ structures-tools/trunk/scripts/generate_moleculeindex.sh
@@ -1,25 +1,15 @@
 #!/bin/sh
 
-cd new 
-
-ls | while read DIR; do
-  if [ -d $DIR ]; then
-    cd $DIR
-    if [ -e molecule_update.xml ]; then
-      rm molecule_update.xml
-    fi
-    ls *cml| while read FILE; do
-      echo $FILE
-      NAME=`echo $FILE |cut -d'.' -f1`
-      IUPAC=`grep IUPAC $FILE | sed -e 's/<[^>]*>//g' -e 's/ //g'`
-      DATE=`date +%Y-%m-%d`
-      echo "  <molecule id=\"CS_${NAME}\">" >> molecule_update.xml
-      echo "    <name>${IUPAC}</name>" >> molecule_update.xml
-      echo "    <filename>${NAME}</filename>" >> molecule_update.xml
-      echo "    <authors>Jerome Pansanel</authors>" >> molecule_update.xml
-      echo "    <date>${DATE}</date>" >> molecule_update.xml
-      echo "  </molecule>" >> molecule_update.xml
-    done
-    cd ..
-  fi
+ls *cml| while read FILE; do
+  echo $FILE
+  NAME=`echo $FILE |cut -d'.' -f1`
+  IUPAC=`grep IUPAC $FILE | sed -e 's/<[^>]*>//g' -e 's/ //g'`
+  DATE=`date +%Y-%m-%d`
+  echo "  <entry id=\"CS_${NAME}\">" >> molecule_update.xml
+  echo "    <name xml:lang=\"en\">${IUPAC}</name>" >> molecule_update.xml
+  echo "    <filename>${NAME}</filename>" >> molecule_update.xml
+  echo "    <authors>Jerome Pansanel</authors>" >> molecule_update.xml
+  echo "    <date>${DATE}</date>" >> molecule_update.xml
+  echo "  </entry>" >> molecule_update.xml
 done
+
