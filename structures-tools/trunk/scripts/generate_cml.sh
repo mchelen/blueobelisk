5c83730787f1b0e3f3c82eb5f90bc099e662cc35
pansanel <pansanel@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-09-17 09:22:38 +0000

    Updating to fit new CML structure

--- structures-tools/trunk/scripts/generate_cml.sh
+++ structures-tools/trunk/scripts/generate_cml.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 
-cd new 
-
-ls | while read DIR; do
-  if [ -d $DIR ]; then
-    cd $DIR
-    ls *gpr| while read FILE; do
-      echo $FILE
-      NAME=`echo $FILE |cut -d'.' -f1`
-      FORMULA=`babel -igpr ${NAME}.gpr -oreport 2> /dev/null | grep "^FORMULA" | sed -e "s/^.*: //"`
-      CML_FORMULA=`mwcalc -p ${FORMULA} | grep "CML Formula" | sed -e "s/^.*://"`
-      MASS=`babel -igpr $FILE -oreport 2> /dev/null |grep '^MASS' | sed -e "s/^.*: //"`
-      EXACT_MASS=`babel -igpr $FILE -oreport 2> /dev/null |grep '^EXACT MASS' | sed -e "s/^.*: //"`
-      babel -igpr ${NAME}.gpr -omol ${NAME}.mol
-      ../../tools/cInChI-1 ${NAME}.mol
-      INCHI_CODE=`cat ${NAME}.mol.txt |grep InChI|cut -d'=' -f2-`
-      babel -igpr ${NAME}.gpr -ocml ${NAME}.cml
-      cat ${NAME}.cml | sed -e "s#<molecule .*>#<molecule xmlns=\"http://www.xml-cml.org/schema/cml2/core\" id=\"CS_${NAME}\">\n <formula concise=\"${CML_FORMULA}\"/>\n <identifier version=\"InChI/1\">\n  <basic>${INCHI_CODE}</basic>\n </identifier>\n <name convention=\"IUPAC\">${NAME}</name>#" -e "s/<\/bondArray>/<\/bondArray>\n <list>\n  <propertyList>\n   <property dictRef=\"cml:molwt\" title=\"molecular weight\">\n    <scalar dataType=\"xsd:decimal\" dictRef=\"cml:molwt\" units=\"unit:g\">${MASS}<\/scalar>\n   <\/property>\n   <property dictRef=\"chemwt:exact_molwt\" title=\"exact molecular weight\">\n    <scalar dataType=\"xsd:decimal\" dictRef=\"chemwt:exact_molwt\" units=\"unit:g\">${EXACT_MASS}<\/scalar>\n   <\/property>\n   <property dictRef=\"cml:mpt\" title=\"melting point\">\n    <scalar dataType=\"xsd:decimal\" errorValue=\"1.0\" dictRef=\"cml:mpt\" units=\"unit:celsius\"><\/scalar>\n   <\/property>\n   <property dictRef=\"cml:bpt\" title=\"boiling point\">\n    <scalar dataType=\"xsd:decimal\" errorValue=\"1.0\" dictRef=\"cml:bpt\" units=\"unit:celsius\"><\/scalar>\n   <\/property>\n  <\/propertyList>\n <\/list>/" > ${NAME}_1.cml 
-      mv ${NAME}_1.cml ${NAME}.cml
-      rm ${NAME}.mol*
-    done
-    cd ..
-  fi
+ls *gpr| while read FILE; do
+  echo $FILE
+  NAME=`echo $FILE |cut -d'.' -f1`
+  FORMULA=`babel -igpr ${NAME}.gpr -oreport 2> /dev/null | grep "^FORMULA" | sed -e "s/^.*: //"`
+  CML_FORMULA=`mwcalc -p ${FORMULA} | grep "CML Formula" | sed -e "s/^.*://"`
+  MASS=`babel -igpr $FILE -oreport 2> /dev/null |grep '^MASS' | sed -e "s/^.*: //"`
+  EXACT_MASS=`babel -igpr $FILE -oreport 2> /dev/null |grep '^EXACT MASS' | sed -e "s/^.*: //"`
+  babel -igpr ${NAME}.gpr -omol ${NAME}.mol
+  INCHI_CODE=`babel -igpr ${NAME}.gpr -oinchi | sed -e's/InChI=//'`
+  babel -igpr ${NAME}.gpr -ocml ${NAME}.cml
+  cat ${NAME}.cml | sed -e "s/<?xml.*/<?xml version=\"1.0\" encoding=\"UTF-8\"?>/"\
+ -e "s#<molecule .*>#<molecule xmlns=\"http://www.xml-cml.org/schema\"\n\
+          xmlns:cml=\"http://www.xml-cml.org/dict/cml\"\n\
+	  xmlns:units=\"http://www.xml-cml.org/units/units\"\n\
+          xmlns:xsd=\"http://www.w3c.org/2001/XMLSchema\"\n\
+          xmlns:iupac=\"http://www.iupac.org\"\n\
+          id=\"CS_${NAME}\">\n\
+  <formula concise=\"${CML_FORMULA}\"/>\n\
+  <identifier convention=\"iupac:inchi\" value=\"${INCHI_CODE}\"/>\n\
+  <name convention=\"IUPAC\">${NAME}</name>#"\
+ -e's/.*<atomA/  <atomA/' \
+ -e's/.*<atom /    <atom /' \
+ -e's/.*<\/atomA/  <\/atomA/' \
+ -e's/.*<bondA/  <bondA/' \
+ -e's/.*<bond /    <bond /' \
+ -e "s/.*<\/bondArray>/  <\/bondArray>\n\
+  <propertyList>\n\
+    <property dictRef=\"cml:molwt\" title=\"Molecular weight\">\n\
+      <scalar dataType=\"xsd:double\" dictRef=\"cml:molwt\" units=\"units:g\">${MASS}<\/scalar>\n\
+    <\/property>\n\
+    <property dictRef=\"cml:monoisotopicwt\" title=\"Monoisotopic weight\">\n\
+      <scalar dataType=\"xsd:double\" dictRef=\"cml:monoisotopicwt\" units=\"units:g\">${EXACT_MASS}<\/scalar>\n\
+    <\/property>\n\
+    <property dictRef=\"cml:mp\" title=\"Melting point\">\n\
+      <scalar dataType=\"xsd:double\" errorValue=\"1.0\" dictRef=\"cml:mp\" units=\"units:celsius\"><\/scalar>\n\
+    <\/property>\n\
+    <property dictRef=\"cml:bp\" title=\"Boiling point\">\n\
+      <scalar dataType=\"xsd:double\" errorValue=\"1.0\" dictRef=\"cml:bp\" units=\"units:celsius\"><\/scalar>\n\
+    <\/property>\n\
+  <\/propertyList>\n/" > ${NAME}_1.cml 
+  mv ${NAME}_1.cml ${NAME}.cml
+  rm ${NAME}.mol*
 done
