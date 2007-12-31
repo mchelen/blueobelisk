ab602a74bd51e1aa19f697bac17be00820d3ee77
egonw <egonw@d3c5a50b-8e31-0410-8099-a759d7a8ce2e>
2007-12-31 13:02:42 +0000

    Handle the new paper preview page of the ACS

--- cb/trunk/handle_acs.pl
+++ cb/trunk/handle_acs.pl
@@ -44,25 +44,36 @@ my @acs_journals = (
 
 foreach my $journal (@acs_journals) {
 	my $sql = $db->prepare("SELECT * FROM papers WHERE journal=? AND ISNULL(title) AND !ISNULL(doi_id)");
+#        my $sql = $db->prepare("SELECT * FROM papers WHERE journal=? AND !ISNULL(doi_id)");
 	$sql->execute($journal);
 	
 	while (my $row = $sql->fetchrow_hashref()) {
 		my $doi = $row->{"doi_id"};
 		my $url = "http://pubs3.acs.org/acs/journals/doilookup?in_doi=".$doi;
-                # print STDERR "URL: $url\n";
+                print STDERR "URL: $url\n";
 		my $page = download_url($url);
 		
                 # print STDERR "$page";
 		print STDERR "Trying DOI $doi\n";
-		if ($page =~ /<span class="textbold">(.*?)<\/span>/) {
-			print STDERR "Got title $1\n";
+#		if ($page =~ /<FONT\sSIZE=+2><B>(.*?)<\/B><\/FONT>/) {
+                if ($page =~ /<ATL><P><FONT\sSIZE=\+2><B>([^<]*)/) {
+                        my $ptitle = $1;
+                        $ptitle =~ s/\n|\r/\s/g;
+                        $ptitle =~ s/\s(\s+)/\s/g;
+                        $ptitle =~ s/^\s*//g;
+                        $ptitle =~ s/\s*$//g;
+			print STDERR "Got title: $ptitle\n";
 			my $update = $db->prepare("UPDATE papers SET title=? WHERE doi_id=?");
-			$update->execute($1, $doi);
+			$update->execute($ptitle, $doi);
+                } elsif ($page =~ /<span class="textbold">(.*?)<\/span>/) {
+                        print STDERR "Got title $1\n";
+                        my $update = $db->prepare("UPDATE papers SET title=? WHERE doi_id=?");
+                        $update->execute($1, $doi);
 		} else {
 			# couldn't get a title for the page, set the title to !NULL, anyway, so that this script won't look at it again.
                         print STDERR "No title found\n";
 			my $update = $db->prepare("UPDATE papers SET title=? WHERE doi_id=?");
-			$update->execute("Unknown ACS publication", $doi);
+			# $update->execute("Unknown ACS publication", $doi);
 		}
 	}
 }
